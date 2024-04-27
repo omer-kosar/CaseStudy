86b8b684dee5328fe5001d3b8e7162de92504939
omer.kosar.33 <omer.kosar.33@gmail.com>
2024-04-27 21:31:24 +0300

    method signature project is added

---
+++ ConsoleApp.MethodSignature/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp.MethodSignature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Show(1, 2);
+            Show(3, 4, 5);
+        }
+
+        private static void Show(params int[] parameters)
+        {
+            Console.WriteLine(string.Join(",", parameters));
+        }
+    }
+}
