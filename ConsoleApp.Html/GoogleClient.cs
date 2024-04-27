8e4f3954ffb7b31bd6707568d5b8a8a75421856f
omer.kosar.33 <omer.kosar.33@gmail.com>
2024-04-27 23:00:46 +0300

    google html code project is added

---
+++ ConsoleApp.Html/GoogleClient.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Html
+{
+    internal class GoogleClient : IDisposable
+    {
+        private ServiceCollection services;
+        private ServiceProvider serviceProvider;
+        private HttpClient client;
+        private IHttpClientFactory httpClientFactory;
+        private readonly string baseUrl = "https://www.google.com";
+        private bool _disposed = false;
+        public GoogleClient()
+        {
+            services = new ServiceCollection();
+            services.AddHttpClient();
+            serviceProvider = services.BuildServiceProvider();
+            httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+            client = httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(baseUrl);
+        }
+
+        public async Task<string> GetHtml()
+        {
+            HttpResponseMessage result = await client.GetAsync("/");
+            if (result.IsSuccessStatusCode)
+            {
+                var content = await result.Content.ReadAsStringAsync();
+                return content;
+            }
+            return string.Empty;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (client != null)
+                    {
+                        client?.Dispose();
+                        serviceProvider?.Dispose();
+                        client = null;
+                        serviceProvider = null;
+                    }
+                }
+
+                _disposed = true;
+            }
+        }
+        ~GoogleClient() => Dispose(false);
+    }
+}
---
+++ ConsoleApp.Html/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Formats.Asn1;
+
+namespace ConsoleApp.Html
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string googleHtmlCode = await GetGoogleHtmlCode();
+            Console.WriteLine(googleHtmlCode);
+        }
+
+        static async Task<string> GetGoogleHtmlCode()
+        {
+            using GoogleClient client = new GoogleClient();
+            return await client.GetHtml();
+        }
+    }
+}
