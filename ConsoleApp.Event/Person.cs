6ee0e92e01d476417f61c029cbd89c62f487aa42
omer.kosar.33 <omer.kosar.33@gmail.com>
2024-04-27 22:15:30 +0300

    person event project is created

---
+++ ConsoleApp.Event/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Event
+{
+    internal class Person : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ConsoleApp.Event/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleApp.Event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.PropertyChanged += Person_PropertyChanged;
+            person.Name = "Ali";
+            person.Name = "Veli";
+        }
+        private static void Person_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Name")
+            {
+                Console.WriteLine("Name özelliği değişti.");
+            }
+        }
+    }
+}
